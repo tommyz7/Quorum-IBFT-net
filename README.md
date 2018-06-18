a6c15171ff1b80d79d3265ee6dd6d446f08968cf
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-06-18 11:11:56 -0700

    created network

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Quorum-IBFT-net
+Quorum network
+==============
+
+This is a setup for quorum network using IBFT consensus. It is based on the
+[7nodes] example.
+
+[7nodes]: https://github.com/jpmorganchase/quorum-examples/tree/master/examples/7nodes
+
+
+How to start
+------------
+
+    vagrant up
+    vagrant ssh
+    cd network
+    ./istanbul-init.sh
+    ./istanbul-start.sh
+
+To stop the network, run `./stop.sh`.
+
+To restart the network, run `./istanbul-init.sh && ./istanbul-start.sh`.
+
+To connect to a node's console, run `geth attach qdata/dd1/geth.ipc` (or `dd2`
+for the 2nd node, etc).
+
+
+Metamask
+--------
+
+Connect metamask to the 'Custom RPC' and specify http://localhost:22000 for
+the 1st node, http://localhost:22001 for the second, ...,
+http://localhost:22006 for the 7th.
+
+Import the keys by clicking on 'Import Account', then 'Select type' to 'JSON
+file', pick one of the keys from [metamask-keys](./metamask-keys/) directory
+and specify password '123'.
+
+*Note*, that current metamask does not send transactions correctly to the
+`geth` nodes of the current quorum. The problem is described in metamask
+[issue-1444](https://github.com/MetaMask/metamask-extension/issues/1444) and
+unfortunately proposed fix only works with the latest `geth` 1.8.11, but the
+quorum provides 1.7.2.
+
+So, metamask currently can query the blockchain, but not send any transaction.
---
+++ network/istanbul-init.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -ue
+
+echo "[*] Cleaning up temporary data directories"
+rm -rf qdata
+mkdir -p qdata/logs
+
+echo "[*] Configuring node 1"
+mkdir -p qdata/dd1/{keystore,geth}
+cp permissioned-nodes.json qdata/dd1/static-nodes.json
+cp permissioned-nodes.json qdata/dd1/
+cp keys/key1 qdata/dd1/keystore
+cp nodes/nodekey1 qdata/dd1/geth/nodekey
+geth --datadir qdata/dd1 init istanbul-genesis.json
+
+echo "[*] Configuring node 2"
+mkdir -p qdata/dd2/{keystore,geth}
+cp permissioned-nodes.json qdata/dd2/static-nodes.json
+cp permissioned-nodes.json qdata/dd2/
+cp keys/key2 qdata/dd2/keystore
+cp nodes/nodekey2 qdata/dd2/geth/nodekey
+geth --datadir qdata/dd2 init istanbul-genesis.json
+
+echo "[*] Configuring node 3"
+mkdir -p qdata/dd3/{keystore,geth}
+cp permissioned-nodes.json qdata/dd3/static-nodes.json
+cp permissioned-nodes.json qdata/dd3/
+cp keys/key3 qdata/dd3/keystore
+cp nodes/nodekey3 qdata/dd3/geth/nodekey
+geth --datadir qdata/dd3 init istanbul-genesis.json
+
+echo "[*] Configuring node 4 as voter"
+mkdir -p qdata/dd4/{keystore,geth}
+cp permissioned-nodes.json qdata/dd4/static-nodes.json
+cp permissioned-nodes.json qdata/dd4/
+cp keys/key4 qdata/dd4/keystore
+cp nodes/nodekey4 qdata/dd4/geth/nodekey
+geth --datadir qdata/dd4 init istanbul-genesis.json
+
+echo "[*] Configuring node 5 as voter"
+mkdir -p qdata/dd5/{keystore,geth}
+cp permissioned-nodes.json qdata/dd5/static-nodes.json
+cp permissioned-nodes.json qdata/dd5/
+cp keys/key5 qdata/dd5/keystore
+cp nodes/nodekey5 qdata/dd5/geth/nodekey
+geth --datadir qdata/dd5 init istanbul-genesis.json
+
+echo "[*] Configuring node 6"
+mkdir -p qdata/dd6/{keystore,geth}
+cp permissioned-nodes.json qdata/dd6/static-nodes.json
+cp permissioned-nodes.json qdata/dd6/
+cp keys/key6 qdata/dd6/keystore
+cp nodes/nodekey6 qdata/dd6/geth/nodekey
+geth --datadir qdata/dd6 init istanbul-genesis.json
+
+echo "[*] Configuring node 7"
+mkdir -p qdata/dd7/{keystore,geth}
+cp permissioned-nodes.json qdata/dd7/static-nodes.json
+cp permissioned-nodes.json qdata/dd7/
+cp keys/key7 qdata/dd7/keystore
+cp nodes/nodekey7 qdata/dd7/geth/nodekey
+geth --datadir qdata/dd7 init istanbul-genesis.json
